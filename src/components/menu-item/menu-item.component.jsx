9b8a6ea3447d65ef2e792c5657ad417395b0f5ec
sandora <sandora@publicisgroupe.net>
2020-06-12 02:53:17 +0530

    Header links and shop component updatd

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,17 @@
 import React from   'react';
 import './menu-item.style.scss';
+import {withRouter} from 'react-router-dom';
 
-const MenuItem = ({title , imageUrl , size}) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({title , imageUrl , size, linkUrl, match, history}) => ( 
+    <div className={`${size} menu-item`}  // eslint-disable-next-line no-restricted-globals
+    onClick={() => history.push(`${match.url}${linkUrl}`)}> 
         <div className= "background-image" style = {{backgroundImage: `url(${imageUrl})`}} />
         <div className='content'>
-            <h1 className='title'>{title. toUpperCase()}</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>shop now</span>
         </div>
     </div>
 
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
